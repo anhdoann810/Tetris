cee66511240bb4d77a86eedc764b153d0ada1fe8
Pham Duc Gia Bao <giabao100206@gmail.com>
2026-06-03 22:33:46 +0700

    refactor: create Constants.java to avoid typos across files + reuse code in GamePanel using drawSingleBlock

---
+++ src/view/Constants.java
@@ -0,0 +1,9 @@
+package view;
+//used in multiple classes so group them here to prevent typo
+
+public class Constants {
+    public static final String SCREEN_MENU = "Menu";
+    public static final String SCREEN_GAME = "MainGame";
+    public static final String SCREEN_HELP = "Help";
+    public static final String SCREEN_DIFFICULTY = "Difficulty";
+}
