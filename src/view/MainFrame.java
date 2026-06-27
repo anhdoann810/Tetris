f83c2b78962aaa0e6a17b210dbbc3751e292c723
plccpllpc2309-dotcom <plccpllpc2309@gmail.com>
2026-06-27 23:36:53 +0700

    Update Class Diagram

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -39,7 +39,6 @@ public class MainFrame extends JFrame {
         this.difficultyPanel = new DifficultyPanel(this.contentPanel, this.cardLayout, this.gameEngine);
 
         this.gamePanel.setEngineAndRouting(this.gameEngine, this.contentPanel, this.cardLayout);
-
         this.gameEngine.setViewUpdater(() -> {
             this.gamePanel.setActivePiece(this.gameEngine.getCurrentPiece());
             this.gamePanel.refreshBoard();
